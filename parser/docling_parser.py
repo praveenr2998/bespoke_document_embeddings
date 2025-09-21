e70e628e4d6e038b25285d1f3e81bf4192228258
praveenr <praveenr2998@gmail.com>
2025-09-21 23:06:50 +0530

    consolidating parsed contents

--- parser/docling_parser.py
+++ parser/docling_parser.py
@@ -2,6 +2,7 @@ import json
 from typing import Any, Literal
 
 from docling.document_converter import DocumentConverter
+from transformers import AutoTokenizer
 
 
 class PDFParser:
@@ -21,11 +22,27 @@ class PDFParser:
         doc = converter.convert(self.pdf_path)
         return doc
 
-    @staticmethod
-    def get_table_of_contents(doc: Any):
-        table_of_contents = []
-        for heading in doc.document.headings:
-            table_of_contents.append(heading)
+    def get_table_of_contents(self, doc: Any):
+        """
+        get the table of contents and page number from the parsed document
+        :param doc:
+        :return:
+        """
+        table_of_contents = {}
+        parsed_dict = self.get_parsed_json(doc)
+        texts = parsed_dict.get("texts", [])
+        for text in texts:
+            if text.get("label") == "section_header":
+                prov = text.get("prov", [])
+                if prov:
+                    start_page = None
+                    for p in prov:
+                        start_page = p.get("page_no")
+                        break
+                    table_of_contents[text.get("text")] = {
+                        "start_page": start_page,
+                        "text_contents": [],
+                    }
         return table_of_contents
 
     @staticmethod
@@ -46,6 +63,30 @@ class PDFParser:
         """
         return doc.document.export_to_markdown()
 
+    def consolidate_text(self, doc: Any):
+        """
+        used to consolidate the all text inside a title/subtitle from the parsed document
+        :param doc:
+        :return:
+        """
+        parsed_dict = self.get_parsed_json(doc)
+        table_of_contents = self.get_table_of_contents(doc)
+        cur_page_header = None
+        for text in parsed_dict.get("texts", []):
+            if text.get("label", "") == "section_header":
+                cur_page_header = text.get("text")
+            if (
+                cur_page_header is not None
+                and text.get("label", "") != "section_header"
+                and text.get("text", "") != ""
+            ):
+                table_of_contents[cur_page_header]["text_contents"].append(
+                    text.get("text")
+                )
+        return table_of_contents
+
+    # def count_tokens(self, input_text: str):
+
     def save(self):
         """
         save the parsed document to a file(json, markdown) in the output path
@@ -54,9 +95,21 @@ class PDFParser:
         doc = self.parse()
         if self.output_type == "json":
             with open(
+                f"{self.output_path}/table_of_contents.json", "w", encoding="utf-8"
+            ) as f:
+                json.dump(
+                    self.get_table_of_contents(doc), f, ensure_ascii=False, indent=4
+                )
+            with open(
                 f"{self.output_path}/parsed_output.json", "w", encoding="utf-8"
             ) as f:
-                json.dump(doc.export_to_dict(), f, ensure_ascii=False, indent=4)
+                json.dump(self.get_parsed_json(doc), f, ensure_ascii=False, indent=4)
+            with open(
+                f"{self.output_path}/consolidated_parsed_output.json",
+                "w",
+                encoding="utf-8",
+            ) as f:
+                json.dump(self.consolidate_text(doc), f, ensure_ascii=False, indent=4)
         elif self.output_type == "markdown":
             raise NotImplemented("Markdown yet to be implemented")
         else:
