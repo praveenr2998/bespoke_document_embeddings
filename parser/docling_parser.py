9a5f90a706ce19acfbf578d3b05dc54a2bb5f1f7
praveenr <praveenr2998@gmail.com>
2025-09-23 09:41:17 +0530

    tokenizer - adjusted content

--- parser/docling_parser.py
+++ parser/docling_parser.py
@@ -1,8 +1,16 @@
 import json
 from typing import Any, Literal
+import os
+from dotenv import load_dotenv
+from huggingface_hub import login
 
 from docling.document_converter import DocumentConverter
 from transformers import AutoTokenizer
+
+load_dotenv()
+if os.getenv("HF_TOKEN") is None:
+    raise ValueError("HF_TOKEN not found in the environment variables")
+login(token=os.getenv("HF_TOKEN"))
 
 
 class PDFParser:
@@ -12,6 +20,9 @@ class PDFParser:
         self.pdf_path = pdf_path
         self.output_path = output_path
         self.output_type = output_type
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            os.getenv("BI_ENCODER_MODEL_NAME"), cache_dir=os.getenv("CACHE_DIR")
+        )
 
     def parse(self):
         """
@@ -85,7 +96,58 @@ class PDFParser:
                 )
         return table_of_contents
 
-    # def count_tokens(self, input_text: str):
+    def count_tokens(self, input_text: str):
+        """
+        used to count the number of tokens in the input text using the model's tokenizer
+        :param input_text: sentence/paragraph
+        :return:
+        """
+        encoded_input = self.tokenizer(input_text)
+        input_ids = encoded_input["input_ids"]
+        token_count = len(input_ids)
+        return token_count
+
+    def form_tokenizer_specific_content(self, parsed_content: dict):
+        """
+        used to adjust the parsed content to fit the context window of the model
+        :param parsed_content:
+        :return:
+        """
+        adjusted_content = {}
+        for title, content in parsed_content.items():
+            text_contents = content.get("text_contents", [])
+            if len(text_contents) == 1:
+                adjusted_content[title] = content
+            else:
+                accumulated_text = ""
+                accumulated_token_count = 0
+                accumulated_text_contents = []
+                spill_over_flag = False
+                for text in text_contents:
+                    token_count = self.count_tokens(text)
+                    if accumulated_token_count + token_count > int(
+                        os.getenv("CONTEXT_WINDOW")
+                    ):
+                        accumulated_text_contents.append(accumulated_text)
+                        accumulated_text = text
+                        accumulated_token_count = token_count
+                        spill_over_flag = True
+                    else:
+                        if accumulated_text == "":
+                            accumulated_text += text
+                        else:
+                            accumulated_text += ". " + text
+                        accumulated_token_count += token_count
+                        spill_over_flag = False
+                if spill_over_flag:
+                    accumulated_text_contents.append(accumulated_text)
+                else:
+                    accumulated_text_contents.append(accumulated_text)
+                adjusted_content[title] = {
+                    "start_page": content.get("start_page"),
+                    "text_contents": accumulated_text_contents,
+                }
+        return adjusted_content
 
     def save(self):
         """
@@ -110,6 +172,21 @@ class PDFParser:
                 encoding="utf-8",
             ) as f:
                 json.dump(self.consolidate_text(doc), f, ensure_ascii=False, indent=4)
+
+            with open(f"{self.output_path}/consolidated_parsed_output.json", "r") as f:
+                parsed_content = json.load(f)
+            with open(
+                f"{self.output_path}/tokenizer_adjusted_parsed_output.json",
+                "w",
+                encoding="utf-8",
+            ) as f:
+                json.dump(
+                    self.form_tokenizer_specific_content(parsed_content=parsed_content),
+                    f,
+                    ensure_ascii=False,
+                    indent=4,
+                )
+
         elif self.output_type == "markdown":
             raise NotImplemented("Markdown yet to be implemented")
         else:
