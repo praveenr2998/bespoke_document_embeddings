c53793db7e77271ba418e35622e7a20b2d15f39c
praveenr <praveenr2998@gmail.com>
2025-09-24 10:21:12 +0530

    README.md

--- main.py
+++ main.py
@@ -22,3 +22,5 @@ if __name__ == "__main__":
             ensure_ascii=False,
             indent=4,
         )
+
+# vllm serve meta-llama/Llama-3.2-3B-Instruct --max-model-len 3000 --max-num-batched-tokens 3000 --dtype auto --api-key praveen@123
