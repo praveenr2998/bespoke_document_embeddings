5a932aceaa01f2813f2619585b5c24285e70e3ac
praveenr <praveenr2998@gmail.com>
2025-09-28 20:52:58 +0530

    adding page number to title

--- main.py
+++ main.py
@@ -7,20 +7,20 @@ if __name__ == "__main__":
     pdf_parser = PDFParser(pdf_path="parser/data/AWQ.pdf", output_path="parser/output/", output_type="json")
     pdf_parser.save()
 
-    with open("parser/output/tokenizer_adjusted_parsed_output.json", "r") as f:
-        parsed_content = json.load(f)
-    generate_questions = GenerateQuestions(parsed_content=parsed_content)
-    parsed_content_with_questions = generate_questions.generate_questions()
-    with open(
-        "question_generator/output/parsed_content_with_questions.json",
-        "w",
-        encoding="utf-8",
-    ) as f:
-        json.dump(
-            parsed_content_with_questions,
-            f,
-            ensure_ascii=False,
-            indent=4,
-        )
+    # with open("parser/output/tokenizer_adjusted_parsed_output.json", "r") as f:
+    #     parsed_content = json.load(f)
+    # generate_questions = GenerateQuestions(parsed_content=parsed_content)
+    # parsed_content_with_questions = generate_questions.generate_questions()
+    # with open(
+    #     "question_generator/output/parsed_content_with_questions.json",
+    #     "w",
+    #     encoding="utf-8",
+    # ) as f:
+    #     json.dump(
+    #         parsed_content_with_questions,
+    #         f,
+    #         ensure_ascii=False,
+    #         indent=4,
+    #     )
 
 # vllm serve meta-llama/Llama-3.2-3B-Instruct --max-model-len 3000 --max-num-batched-tokens 3000 --dtype auto --api-key praveen@123
